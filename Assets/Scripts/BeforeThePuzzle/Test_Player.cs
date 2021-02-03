fb23a85598877aa65a832981704d69620f68a4a6
hanasimorio <78461761+hanasimorio@users.noreply.github.com>
2021-02-03 23:41:55 +0900

    ピンを動かす

--- Assets/Scripts/BeforeThePuzzle/Test_Player.cs
+++ Assets/Scripts/BeforeThePuzzle/Test_Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Test_Player : MonoBehaviour
 {
-    float speed = 250;
+    float speed = 300;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,20 +26,6 @@ public class Test_Player : MonoBehaviour
             if (-800 < transform.position.x)
             {
                 transform.position = transform.position - new Vector3(speed, 0, 0) * Time.deltaTime;
-            }
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            if (transform.position.y < 800)
-            {
-                transform.position = transform.position + new Vector3(0, speed, 0) * Time.deltaTime;
-            }
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            if (-800 < transform.position.y)
-            {
-                transform.position = transform.position - new Vector3(0, speed, 0) * Time.deltaTime;
             }
         }
     }
