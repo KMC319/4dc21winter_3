3bcaf3f71ff727576ffd18f52446a6608d86900b
Gakutocit <53819950+Gakutocit@users.noreply.github.com>
2021-02-05 18:30:50 +0900

    p

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,31 +4,54 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rigid2d;
+
+    public GroundCheck ground;
 
     public float speed = 8f;
     public float moveableRange = 5.5f;
     public float jumppower = 10f;
     public float accel = 0.0f;
-    private Vector2 velocity;
+
+    private Animator anim = null;
+    private Rigidbody2D rb = null;
     [SerializeField] ContactFilter2D filter2d;
 
+    void Start()
+    {
+        anim = GetComponent<Animotor>();
+        rb = GetComponent < Rigidbody2D.();
+    }
 
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //move
-        transform.Translate(Input.GetAxisRaw(
-            "Horizontal") * speed * Time.deltaTime, 0, 0);
-        transform.position = new Vector2(Mathf.Clamp(
-            transform.position.x, -moveableRange, moveableRange),
-            transform.position.y);
+        //move and stay
+        float horizontalKey = Input.GetAxis("Horizontal");
+        float xSpeed = 0.0f;
+        if(horizontalKey>0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            anim.SetBool("move",true);
+            xSpeed = speed + accel;
+        }
+        else if (horizontalKey < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            anim.SetBool("move", true);
+            xSpeed = -speed - accel;
+        }
+        else
+        {
+            anim.SetBool("move", false);
+            xSpeed = 0.0f;
+        }
+        rb.velocity = new Vector2(xSpeed, rb.velocity.y)
 
         //jump
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            rigid2d.AddForce(transform.up * (jumppower + accel));
+            rigid2d.AddForce(transform.up * jumppower);
         }
 
         //jump length reset
