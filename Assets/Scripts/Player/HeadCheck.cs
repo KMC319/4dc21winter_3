d8221dd9b08074be0c76bf5ac6537b78dadce741
Gakutocit <53819950+Gakutocit@users.noreply.github.com>
2021-02-05 21:09:12 +0900

    a

---
+++ Assets/Scripts/Player/HeadCheck.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//player no  atama ni  korewo ireta object wo secchi
+
+public class GroundCheck : MonoBehaviour
+{
+    private string groundTag = "Ground";
+    private bool isGround = faise;
+    private bool isGroundEnter, isGroundStay, isGroundExit;
+    //returning method to judge
+    public bool IsGround()
+    {
+        if (isGroundEnter || isGroundStay)
+        {
+            isGround = true;
+        }
+        else if (isGroundExit)
+        {
+            isGround = false;
+        }
+        isGroundEnter = false;
+        isGroundStay = false;
+        isGroundExit = false;
+        return isGround;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == groundTag)
+        {
+            isGroundEnter = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == groundTag)
+        {
+            isGroundStay = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == groundTag)
+        {
+            isGroundExit = true;
+        }
+    }
+
+
+}
