252d231cd67f2868c406b46907ee583189f42b8d
KMC319 <31245129+KMC319@users.noreply.github.com>
2021-02-06 13:43:47 +0900

    hiscore save

--- Assets/Scripts/ProjectSystem/ScoreManager.cs
+++ Assets/Scripts/ProjectSystem/ScoreManager.cs
@@ -1,9 +1,23 @@
+using Action;
+using UnityEngine;
+
 namespace ProjectSystem {
     public static class ScoreManager {
         public static float HiScore { get; private set; }
         public static float PastScore { get; private set; } = 0;
-        public static float CurrentScore { get; private set; } = 0;
+        public static float CurrentScore => PuzzleGameManager.Chicken_count * 59472 - Timer.GameTime * 5000;
+        private static string key = "hiScore";
         
-        
+        public static void Save() {
+            PastScore = CurrentScore;
+            if (HiScore <= CurrentScore) {
+                HiScore = CurrentScore;
+                PlayerPrefs.SetFloat(key, CurrentScore);
+            }
+        }
+
+        static ScoreManager() {
+            HiScore = PlayerPrefs.GetFloat(key, 0);
+        }
     }
 }
