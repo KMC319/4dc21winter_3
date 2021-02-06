b1e9ddf15c6cb4ed054db8ea0c68131eba53d838
hanasimorio <78461761+hanasimorio@users.noreply.github.com>
2021-02-06 12:50:48 +0900

    Update GameOver.cs

--- Assets/Scripts/GameOver/GameOver.cs
+++ Assets/Scripts/GameOver/GameOver.cs
@@ -12,9 +12,18 @@ public class GameOver : MonoBehaviour
     public GameObject Cool_text;
     private static readonly string[] COMMENTS = new string[] {"飛べない鶏はただの鶏肉だ\nA chicken that can't fly is just a chicken. ",
     "そこのお前！鶏肉100gあたりに含まれる脂質は鶏肉100g分だぜ\nHey, you! That's 100 grams of fat in every 100 grams of chicken.",
-        "で、俺が料理されたってわけ\nAnd then they cooked me.","鶏肉、下から見るか横から見るか\nChicken, viewed from below or from the side",
-        "鳥柱、鶏肉の呼吸、壱の型、鶏！！\nBird pillar, chicken breath, type one, chicken!","長男だったら我慢できたけど、次男だから我慢できなかった\nI could  was my first son, but I couldn't because my second son.",
-    "PUI PUI PUI ",};
+    "で、俺が料理されたってわけ\nAnd then they cooked me.",
+    "鶏肉、下から見るか横から見るか\nChicken, viewed from below or from the side",
+    "鳥柱、鶏肉の呼吸、壱の型、鶏！！\nBird pillar, chicken breath, type one, chicken!",
+    "長男だったら我慢できたけど、次男だから我慢できなかった\nI could  was my first son, but I couldn't because my second son.",
+    "PUI PUI PUI ",
+    "まったく鶏肉料理は最高だぜ！！\nI've never had a better chicken dish!",
+    "鶏じゃダメなんですか？？？\nWhy not a chicken???",
+    "あんたは肉とここで死ぬのよ\nYou'll die here with the meat.",
+    "鶏が死ぬわけでしょ。それさーもうパァワァハァラァだよね～～\nThe chicken dies, right? That's so pah-wah-ha-la-la!",
+    "おそろしく速いGameOver..オレでなきゃ見逃しちゃうね\nGameOver is so fast, I'd have missed it.",
+    "流石に高スコアは取りにくいw鶏肉だけに\nIt's hard to get a high score on a quirky w chicken only.",
+    };
     // Start is called before the first frame update
     void Start()
     {
