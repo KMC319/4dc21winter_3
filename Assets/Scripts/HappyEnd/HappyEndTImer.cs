3a9ddc98334a9abe8097abafa84824d9344afe23
T4-C <60639617+T4-C@users.noreply.github.com>
2021-02-06 14:32:09 +0900

    a

--- Assets/Scripts/HappyEnd/HappyEndTImer.cs
+++ Assets/Scripts/HappyEnd/HappyEndTImer.cs
@@ -9,11 +9,18 @@ public class HappyEndTImer : MonoBehaviour
     public AudioClip sound1;
     AudioSource audioSource;
 
+    public GameObject Chicken1;
+    public GameObject Chicken2;
+    public GameObject Chicken3;
+
+    int killCount;
+
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         Invoke("ShowText",3f);
+        killCount = PuzzleGameManager.Death_count;
     }
 
     // Update is called once per frame
@@ -26,5 +33,15 @@ public class HappyEndTImer : MonoBehaviour
         audioSource.Play();
         audioSource.loop = true;
         mainText.SetActive(true);
+        if(killCount<=3)
+        {
+            Chicken1.SetActive(true);
+        }else if(killCount<=6)
+        {
+            Chicken2.SetActive(true);
+        }else
+        {
+            Chicken3.SetActive(true);
+        }
     }
 }
